4f74ee1d9acafa677aa299117cb4cf0b0939fbb4
Khaled-Ballour <ballourk@gmail.com>
2023-03-28 14:49:51 +0200

    adding the ColorList Component

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,4 +1,13 @@
-const ColorList = () => {
-  return <div>ColorList</div>;
+import Color from './Color';
+import { nanoid } from 'nanoid';
+
+const ColorList = ({ colors }) => {
+  return (
+    <section className="colors">
+      {colors.map((color, index) => {
+        return <Color color={color} key={nanoid()} index={index} />;
+      })}
+    </section>
+  );
 };
 export default ColorList;
