54e068eca82574b4bc156ffcf9a8db7713d6d97b
Khaled-Ballour <ballourk@gmail.com>
2023-03-28 15:40:52 +0200

    implementing the toast messages

--- src/Color.jsx
+++ src/Color.jsx
@@ -1,9 +1,24 @@
+import { toast } from 'react-toastify';
+
 const Color = ({ index, color }) => {
   const { hex, weight } = color;
+  const saveToClick = async (e) => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success('Color copied to clipboard');
+      } catch (error) {
+        toast.error('Field to copy to clipboard');
+      }
+    } else {
+      toast.error('Clipboard access not available');
+    }
+  };
   return (
     <article
       className={index > 10 ? 'color color-light' : 'color'}
       style={{ background: `#${hex}` }}
+      onClick={saveToClick}
     >
       <p className="percent-value">{weight}%</p>
       <p className="percent-value">#{hex}</p>
