371dbaa39de9f837f7e2d21a20ee2378cd3a577b
Khaled-Ballour <ballourk@gmail.com>
2023-03-28 15:02:24 +0200

    added the color component

--- src/Color.jsx
+++ src/Color.jsx
@@ -1,4 +1,13 @@
-const Color = () => {
-  return <div>Color</div>;
+const Color = ({ index, color }) => {
+  const { hex, weight } = color;
+  return (
+    <article
+      className={index > 10 ? 'color color-light' : 'color'}
+      style={{ background: `#${hex}` }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="percent-value">#{hex}</p>
+    </article>
+  );
 };
 export default Color;
